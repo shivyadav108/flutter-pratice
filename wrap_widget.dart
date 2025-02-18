53a0b6d699a1abe49dde3616f60cbfbc1583a4f2
Shivshankar kumar yadav <167058626+shivyadav108@users.noreply.github.com>
2025-02-18 17:42:41 +0530

    Create wrap_widget.dart
    without scrolling horizental mai data

---
+++ wrap_widget.dart
@@ -0,0 +1,17 @@
+Wrap(
+        // direction: Axis.vertical,
+        spacing: 11,
+        runSpacing: 11,
+        children: [
+          Container(width: 100, height: 100, color: Colors.blue,),
+          Container(width: 100, height: 100, color: Colors.brown,),
+          Container(width: 100, height: 100, color: Colors.orange,),
+          Container(width: 100, height: 100, color: Colors.blueGrey,),
+          Container(width: 100, height: 100, color: Colors.black12,),
+          Container(width: 100, height: 100, color: Colors.blue,),
+          Container(width: 100, height: 100, color: Colors.blue,),
+          Container(width: 100, height: 100, color: Colors.blue,),
+          Container(width: 100, height: 100, color: Colors.blue,),
+          Container(width: 100, height: 100, color: Colors.blue,),
+        ],
+      )
